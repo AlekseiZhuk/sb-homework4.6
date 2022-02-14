376080fe913e41721640bd4fbb841de939398c0c
Zhuk Alexey <vizuwi@mail.ru>
2022-02-14 18:39:48 +0300

    complete Task1

--- 04_StatefulStatelessWidget/list_view_keys_scroll/lib/main.dart
+++ 04_StatefulStatelessWidget/list_view_keys_scroll/lib/main.dart
@@ -27,7 +27,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> nav = ['Список 1', 'Список 2'];
+  final List<String> nav = ['Мои фото', 'Галерея'];
   final List fakeData = List.generate(100, (index) => index.toString());
 
   Map<String, dynamic> data = {
@@ -64,14 +64,16 @@ class _MyHomePageState extends State<MyHomePage> {
       child: Scaffold(
         appBar: AppBar(
           title: Text('Homework example'),
+          bottom:
+              TabBar(tabs: nav.map((String item) => Tab(text: item)).toList()),
         ),
         body: TabBarView(
           children: nav.map((name) {
             return ListView(
               key: PageStorageKey(name),
               children: <Widget>[
-                ...fakeData.map((e) {
-                  return Text(e);
+                ...data[name].map((e) {
+                  return Image.network(e.toString());
                 }).toList()
               ],
             );
